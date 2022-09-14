158b6f21a222b0a6d44f81ea678fc6dff7e5a1bd
jadewfoster <jadewfoster@gmail.com>
2022-09-14 12:51:02 +0100

    Profiles

--- src/main/java/com/qa/demo/persistence/domain/AppConfig.java
+++ src/main/java/com/qa/demo/persistence/domain/AppConfig.java
@@ -2,9 +2,11 @@ package com.qa.demo.persistence.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.modelmapper.ModelMapper;
 
 @Configuration
+@Profile({"dev", "prod"})
 	public class AppConfig {
 
 	    @Bean
